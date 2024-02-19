1297b9888e228113b1f4408d7c8866e876d3a240
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-02-19 08:50:30 +0000

    primo commit

---
+++ angular-gitpod/src/app/primo/primo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primo',
+  standalone: true,
+  imports: [],
+  templateUrl: './primo.component.html',
+  styleUrl: './primo.component.css'
+})
+export class PrimoComponent {
+
+}
